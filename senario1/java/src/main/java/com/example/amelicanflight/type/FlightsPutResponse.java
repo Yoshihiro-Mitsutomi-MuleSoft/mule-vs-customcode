dc4a1cb6ce851875eff5a3ceeccf5bc551324241
tmiya4ta <tmiyashita@salesforce.com>
2024-01-18 14:03:02 +0900

    Java: find updated keys in Plane

--- senario1/java/src/main/java/com/example/amelicanflight/type/FlightsPutResponse.java
+++ senario1/java/src/main/java/com/example/amelicanflight/type/FlightsPutResponse.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 /**
@@ -19,11 +21,34 @@ public class FlightsPutResponse {
     DtoFlight before;
     DtoFlight after;
 
+    public void getDiffs(Map<String, Object> aMap, Map<String, Object> bMap, List<String> path, List<String> updateKeys) {
+
+        aMap.forEach((key, value) -> {
+            if(HashMap.class.isInstance(value)) {
+                path.add(key);
+                getDiffs((Map<String, Object>)value, bMap, path, updateKeys);
+                path.remove(path.size() - 1);
+            } else {
+              Map<String, Object> ref = bMap;
+              for (String p : path) {
+                ref = (Map<String, Object>)ref.get(p);
+              }
+              if (!(value.equals(ref.get(key)))) {
+                path.add(key);
+                updateKeys.add(String.join(".", path));
+                path.remove(path.size() - 1);
+              }
+            }
+        });
+    }
     public FlightsPutResponse(DtoFlight aBefore, DtoFlight aAfter) {
         Map<String, Object> bm = aBefore.asMap();
         Map<String, Object> am = aAfter.asMap();
-        updatedKeys = am.keySet().stream().filter(k -> !(bm.get(k).equals(am.get(k)))).collect(Collectors.toList());
+        updatedKeys = new ArrayList();
+        List<String> path = new ArrayList<String>();
+        getDiffs(am, bm, path, updatedKeys);
         before = aBefore;
         after = aAfter;
+       
     }
 }
